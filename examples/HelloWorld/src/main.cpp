f99fe397abf9962f1aa0cf544daa9d1a5769c9ba
Junior Rantila <junior.rantila@gmail.com>
2023-02-20 09:59:40 +0100

    HelloWorld: Add TEST_CASE example

--- examples/HelloWorld/src/main.cpp
+++ examples/HelloWorld/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Core/Print.h>
 #include <Main/Main.h>
+#include <Unit/Unit.h>
 
 ErrorOr<int> Main::main(int argc, c_string argv[])
 {
@@ -9,4 +10,12 @@ ErrorOr<int> Main::main(int argc, c_string argv[])
     writeln("Hello, World!"sv);
 
     return 0;
+}
+
+TEST_CASE(main)
+{
+    auto buffer = SHOULD(StringBuffer::create_fill("Hello"sv));
+    SHOULD(buffer.write(", World!"sv));
+    EXPECT(buffer.view() == "Hello, World!"sv);
+    return {};
 }
