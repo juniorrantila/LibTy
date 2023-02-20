8b073de1dab08b8bb809bdcf188b23c8de7c1ce9
Junior Rantila <junior.rantila@gmail.com>
2023-02-20 10:18:41 +0100

    HelloWorld: Add a slow test to show a bit more of how TestRunner works

--- examples/HelloWorld/src/main.cpp
+++ examples/HelloWorld/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Core/Print.h>
+#include <Core/System.h>
 #include <Main/Main.h>
 #include <Unit/Unit.h>
 
@@ -17,5 +18,11 @@ TEST_CASE(main)
     auto buffer = SHOULD(StringBuffer::create_fill("Hello"sv));
     SHOULD(buffer.write(", World!"sv));
     EXPECT(buffer.view() == "Hello, World!"sv);
+    return {};
+}
+
+TEST_CASE(slow_test)
+{
+    Core::System::sleep(2);
     return {};
 }
