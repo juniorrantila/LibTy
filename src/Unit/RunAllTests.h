46f7855999450d42e7a5cd67b9b4f802c3a5261d
Junior Rantila <junior.rantila@gmail.com>
2023-06-22 14:31:21 +0200

    Unit: Remove verbose rename
    The thought of this was to make it easier to ensure the name in the
    binary being specific for macos, but it seems like it was not needed.

--- src/Unit/RunAllTests.h
+++ src/Unit/RunAllTests.h
@@ -4,6 +4,6 @@
 #define UNIT_TEST_RUNNER_NAME "unit_run_all_tests"
 extern "C" {
 
-extern void unit_run_all_tests(Unit::TestContext) asm(
-    UNIT_TEST_RUNNER_NAME);
+void unit_run_all_tests(Unit::TestContext);
+
 }
